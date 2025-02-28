8ce6e85b8edd54e6b7927a7f98424bef9c9c91a2
Johnny Herrera <jmjaramillo95@gmail.com>
2025-02-27 23:09:16 -0500

    crud

---
+++ Models/Pizza.cs
@@ -0,0 +1,8 @@
+namespace ApiWeb.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool? IsGlutenFree { get; set; }
+}
--- obj/Debug/net8.0/ApiWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820a2ebcdd1167ba40c6e3b20222b8f8c2b0ddcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08256618f003822932698754e2fc9eed93484c29")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
